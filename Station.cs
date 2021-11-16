b7638f71d28c7248a34a47f0cc1f2f985ddcbd73
khanhn <khanh95hd@gmail.com>
2021-11-16 08:53:07 +0700

    update

---
+++ Station.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent2._0
+{
+    class Station
+    {
+        
+        string Name { get; set; }
+        string FolderName { get; set; }
+        Int16 Code { get; set; }
+        Type Type { get; set; }
+    }
+    enum Type
+    {
+        Device,
+        DeviceResult,
+        DetailResult
+    }
+}
